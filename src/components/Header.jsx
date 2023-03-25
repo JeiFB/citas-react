36ddd1f1465f859ac733b6e0e90c0ea285733fd0
JeiFB <fuentesjeifer@gmail.com>
2023-03-25 18:07:02 -0500

    espacio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(){
 
     return (
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Veterinaria de{""}
+            Veterinaria de {""}
             <span className="text-indigo-600">jeifher</span>
         </h1>
     )
